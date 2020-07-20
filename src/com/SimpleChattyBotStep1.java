9426149b21fac72ec0639b116114838312a3b044
Jingwen Pourpoint <jingwen.pourpoint@gmail.com>
2020-07-20 17:54:08 +0200

    All steps of SimpleChattyBot project from JetBrains Academy Java courses

---
+++ src/com/SimpleChattyBotStep1.java
@@ -0,0 +1,17 @@
+package com;
+
+public class SimpleChattyBotStep1 {
+  //  private static final String name = "Aid";
+  //  private static final int year = 2020;
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+
+
+        //  System.out.printf("Hello! My name is %s.\n",name);
+        // System.out.printf("I was created in %d. \n",year);
+
+    }
+}
+
+
