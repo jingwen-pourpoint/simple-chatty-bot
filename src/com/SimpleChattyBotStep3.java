0e28be1bf3eee423f230b09832beef1907d29b09
Jingwen Pourpoint <jingwen.pourpoint@gmail.com>
2020-08-09 19:32:17 +0200

    All steps of SimpleChattyBot project from JetBrains Academy Java courses

--- src/com/SimpleChattyBotStep3.java
+++ src/com/SimpleChattyBotStep3.java
@@ -20,13 +20,7 @@ public class SimpleChattyBotStep3 {
         int remainder5 = scanner.nextInt();
         int remainder7 = scanner.nextInt();
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-        System.out.println("Your age is " + age + " ; that's a good time to start programming!");
 
-        System.out.println("Now I will prove to you that I can count to any number you want.");
-        int number = scanner.nextInt();
-        for (int i = 0; i <= number ; i++){
-            System.out.println(i + "!");
-        }
-        System.out.println("Completed, have a nice day!");
+        System.out.println("Your age is " + age + " ; that's a good time to start programming!");
     }
 }
